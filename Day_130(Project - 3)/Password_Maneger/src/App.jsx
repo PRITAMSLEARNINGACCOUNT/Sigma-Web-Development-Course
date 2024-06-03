90acda2a9afc759934a669456d3442b042c04904
pritamstech <pritamstech@hotmail.com>
2024-06-03 09:30:58 +0530

    Project Password Maneger Started

---
+++ Day_130(Project - 3)/Password_Maneger/src/App.jsx
@@ -0,0 +1,41 @@
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Password_Maneger from "./Components/Password_Maneger";
+import Mode from "./Context/Light_Dark_Mode";
+import PassContext from "./Context/Passwords";
+import { useState, useEffect } from "react";
+// import { ToastContainer } from "react-toastify";
+function App() {
+  const [ToggleMode, setMode] = useState(false);
+  const [Passwords, setPasswords] = useState([]);
+  useEffect(() => {
+    if (localStorage.getItem("Passwords")) {
+      const Pass = JSON.parse(localStorage.getItem("Passwords"));
+      // console.log(Pass);
+      if (Pass !== null) {
+        setPasswords(Pass);
+      }
+    }
+  }, []);
+  return (
+    <>
+      <PassContext.Provider value={{ Passwords, setPasswords }}>
+        <Mode.Provider value={{ ToggleMode, setMode }}>
+          <Navbar />
+          {ToggleMode ? (
+            <div className="[background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#63e_100%)]">
+              <Password_Maneger />
+            </div>
+          ) : (
+            <div className="[background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+              <Password_Maneger />
+            </div>
+          )}
+          <div></div>
+        </Mode.Provider>
+      </PassContext.Provider>
+    </>
+  );
+}
+
+export default App;
