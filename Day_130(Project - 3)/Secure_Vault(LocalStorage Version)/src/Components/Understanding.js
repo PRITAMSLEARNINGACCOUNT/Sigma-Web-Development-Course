823d121cbf1c5bce38c2ead1770516f351eac8d2
pritamstech <pritamstech@hotmail.com>
2024-07-17 00:51:57 +0530

    Mongod With Hosting Fundamentals Done

--- Day_130(Project - 3)/Secure_Vault(LocalStorage Version)/src/Components/Understanding.js
+++
@@ -1,8 +0,0 @@
-let arr = [];
-let obj = {
-  key: "Pritam",
-  key2: "Pritam",
-  key3: "Pritam",
-};
-console.log(typeof JSON.stringify([...arr, obj]));
-console.log(typeof JSON.parse(JSON.stringify([...arr, obj, obj])));
--- Day_130(Project - 3)/Secure_Vault(Mongodb Version)/src/Context/Passwords.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-const PassContext = createContext();
-export default PassContext;
